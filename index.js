7f4c3388e13f920bde00d349b621baf4c2ac8ff3
Nishu0 <itsthakkarnisarg@gmail.com>
2023-03-05 10:39:29 +0530

    Spotify Youtube Trending List

--- index.js
+++ index.js
@@ -68,7 +68,124 @@ app.get('/login',(req,res)=>{
 	res.render('login.ejs')
 })
 
+app.get('/videos', async (req, res) => {
+        try {
+          const response = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
+            params: {
+              part: 'snippet,statistics',
+              chart: 'mostPopular',
+              regionCode: 'IN',
+              maxResults: 12,
+              key: process.env.YOUTUBE_API_KEY // Replace with your own YouTube API key
+            }
+          });
+      
+          const videos = response.data.items.map(item => ({
+            title: item.snippet.title,
+            description: item.snippet.description,
+            thumbnailUrl: item.snippet.thumbnails.default.url,
+            viewCount: item.statistics.viewCount,
+            likeCount: item.statistics.likeCount,
+            dislikeCount: item.statistics.dislikeCount,
+            embedUrl: `https://www.youtube.com/embed/${item.id}`
+          }));
+      
+          // Render the EJS template with the videos data
+          res.render('videos', { videos });
+        } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+});
 
+app.get('/videos/trending', async (req, res) => {
+  try {
+    const response = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
+      params: {
+        part: 'snippet,statistics',
+        chart: 'mostPopular',
+        regionCode: 'US',
+        maxResults: 10,
+        key: process.env.YOUTUBE_API_KEY // Replace with your own YouTube API key
+      }
+    });
+
+    const videos = response.data.items.map(item => ({
+      id: item.id,
+      title: item.snippet.title,
+      description: item.snippet.description,
+      thumbnailUrl: item.snippet.thumbnails.default.url,
+      viewCount: item.statistics.viewCount,
+      likeCount: item.statistics.likeCount,
+      dislikeCount: item.statistics.dislikeCount,
+      embedUrl: `https://www.youtube.com/embed/${item.id}`,
+      isFavorite: false // Initialize isFavorite to false for all videos
+    }));
+
+    // Render the EJS template with the videos data
+    res.render('videos', { videos });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/videos/:id/favorite', (req, res) => {
+  const videoId = req.params.id;
+  const isFavorite = req.body.isFavorite;
+
+  // Here you can update the database or perform any other operation to save the isFavorite status
+  // For demo purposes, we will just update the video object in the videos array
+  videos.forEach(video => {
+    if (video.id === videoId) {
+      video.isFavorite = isFavorite;
+    }
+  });
+
+  res.status(200).send('OK');
+});
+
+app.get('/music', async (req, res) => {
+    try {
+      // Get an access token from Spotify API
+      const authResponse = await axios.post('https://accounts.spotify.com/api/token', 
+        'grant_type=client_credentials', {
+          headers: {
+            'Authorization': `Basic ${Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64')}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+      });
+  
+      const accessToken = authResponse.data.access_token;
+  
+      // Fetch trending tracks from Spotify API
+      const response = await axios.get('https://api.spotify.com/v1/browse/new-releases', {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+          'Content-Type': 'application/json'
+        },
+        params: {
+          country: 'US',
+          limit: 12
+        }
+      });
+  
+      const tracks = response.data.albums.items.map(item => ({
+        name: item.name,
+        artist: item.artists[0].name,
+        image: item.images[0].url,
+        releaseDate: item.release_date,
+        previewUrl: item.external_urls.spotify
+      }));
+  
+      // Render the JSON response with the tracks data
+      //res.json(tracks);
+      res.render('songs', { songs: tracks });
+    }catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Unable to fetch trending Spotify music.' });
+    }
+  });
 
 
 app.get('/register',(req,res)=>{
